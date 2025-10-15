8d80d0121281c514f89ff94b20d16a59cf8dae18
Alex Panteli <alekspanteli@gmail.com>
2025-10-15 14:29:23 +0300

    Implement dashboard enhancements by adding revenue and latest invoices components, and updating the page layout. Uncomment relevant sections for future use.

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -10,7 +10,7 @@ export default async function LatestInvoices({ latestInvoices }: { latestInvoice
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -47,7 +47,7 @@ export default async function LatestInvoices({ latestInvoices }: { latestInvoice
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pt-6 pb-2">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
