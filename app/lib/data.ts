96cca5dbb62fa66a0407f3b225d3ff98706696cb
Alex Panteli <alekspanteli@gmail.com>
2025-10-24 12:29:38 +0300

    Add 404 Not Found page for invoice editing; implement server-side notFound handling in page component.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -146,6 +146,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }))
 
+    console.log(invoice);
     return invoice[0]
   } catch (error) {
     console.error('Database Error:', error)
