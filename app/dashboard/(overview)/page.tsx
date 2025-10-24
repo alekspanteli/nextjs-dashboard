524b0172e8c550f8fa48be248eeaf1a7fee57a09
Alex Panteli <alekspanteli@gmail.com>
2025-10-24 12:49:30 +0300

    Refactor ESLint configuration to use defineConfig and globalIgnores; add linting scripts to package.json; clean up unused imports in dashboard and login components.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,5 +1,4 @@
 import { lusitana } from '@/app/ui/fonts'
-import { fetchCardData, fetchLatestInvoices } from '@/app/lib/data'
 import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
 import CardWrapper from '@/app/ui/dashboard/cards'
@@ -7,7 +6,6 @@ import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/a
 import { Suspense } from 'react'
 
 export default async function Page() {
-  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData()
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
